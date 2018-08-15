2b87a8c924b323a3acda19779843054fed6c7c20
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-08-15 12:22:48 -0500

    Started with initial files and implemented some starter variables

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var friends = ['Melissa', 'Amy', 'Matt', 'Brennan', 'John'];
+    var locations = ['Conservatory', 'Lounge', 'Kitchen', 'Library', 'Hall', 'Study', 'Ballroom', 'Dining Room', 'Billiard Room', 'Server Room'];
+    var weapons = [
+        'a door','a tea kettle','a stop sign', 'a crock pot', 'some thick book about mastering C from the 80s',
+        'a Yu-gi-oh Duel Deck', 'a pair of Heelys', 'a Tamagotchi', 'Lawn Darts', 'a Furby',
+        'a Bop-It', 'a Sony Walkman', 'a board game of Clue', 'an IBM Model M Keyboard', 'a Tesla Model 3',
+        'a first-aid kit (in an ironic twist of events)', 'an AOL Disc', 'an Easy Bake Oven', 'Leprechaun 6: Back 2 tha Hood on DVD', 'a wild dingo'
+    ];
+
+   // this.append( /* h3 loop */)
+});
