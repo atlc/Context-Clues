80b2eaccc180a889de31885e64b1236b10e648b2
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-08-16 07:53:58 -0500

    Each accusation when clicked will release the full accusation statement

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var friends = ['Melissa', 'Amy', 'Matt', 'Brennan', 'John'];
-    var locations = ['Conservatory', 'Lounge', 'Kitchen', 'Library', 'Hall', 'Study', 'Ballroom', 'Dining Room', 'Billiard Room', 'Server Room'];
+    var locations = [
+        'Conservatory', 'Lounge', 'Kitchen', 'Library', 'Hall',
+        'Study', 'Ballroom', 'Dining Room', 'Billiard Room', 'Server Room'
+    ];
     var weapons = [
-        'a door','a tea kettle','a stop sign', 'a crock pot', 'some thick book about mastering C from the 80s',
+        'a door', 'a tea kettle', 'a stop sign', 'a crock pot', 'some thick book about mastering C from the 80s',
         'a Yu-gi-oh Duel Deck', 'a pair of Heelys', 'a Tamagotchi', 'Lawn Darts', 'a Furby',
         'a Bop-It', 'a Sony Walkman', 'a board game of Clue', 'an IBM Model M Keyboard', 'a Tesla Model 3',
         'a first-aid kit (in an ironic twist of events)', 'an AOL Disc', 'an Easy Bake Oven', 'Leprechaun 6: Back 2 tha Hood on DVD', 'a wild dingo'
     ];
 
-   // this.append( /* h3 loop */)
+    for (var i = 1; i <= 100; i++) {
+        var accusation = ('<h3 id=\'' + i +  '\'>Accusation ' + i + '</h3>');
+        $('body').append(accusation);
+        $('#'+i).click(displayAccusation(i));
+    }
+
+
+    function displayAccusation(i) {
+        return function () {
+            alert('I accuse ' + friends[(friends.length > i ? i : i % friends.length)] 
+            + ', with ' + weapons[(weapons.length > i ? i : i % weapons.length)] 
+            + ' in the ' + locations[(locations.length > i ? i : i % locations.length)]);
+        }
+    };
 });
